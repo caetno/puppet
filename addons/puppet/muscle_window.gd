44798cd82380773659a706909d4ca0e331a6873e
caetno <caetno@users.noreply.github.com>
2025-08-23 21:09:25 +0200

    feat: add 3d muscle editor layout

--- addons/puppet/muscle_window.gd
+++ addons/puppet/muscle_window.gd
@@ -5,24 +5,64 @@ extends Window
 var editor_plugin: EditorPlugin
 
 const MuscleData = preload("res://addons/puppet/muscle_data.gd")
-const MuscleIO = preload("res://addons/puppet/io.gd")
 const MuscleProfile = preload("res://addons/puppet/profile_resource.gd")
+const HumanoidScene = preload("res://humanoid_example.tscn")
 
 var _profile: MuscleProfile = MuscleProfile.new()
+var _model: Node3D
 
-@onready var _list: VBoxContainer = $VBox/Scroll/List
-@onready var _load_dialog: FileDialog = $LoadDialog
-@onready var _save_dialog: FileDialog = $SaveDialog
+@onready var _list: VBoxContainer = $VBox/Main/Right/List
+@onready var _tree: Tree = $VBox/Main/Left/NodeTree
+@onready var _viewport: SubViewport = $VBox/Main/ViewportPane/SubViewport
+@onready var _picker: EditorResourcePicker = $VBox/Top/ProfilePicker
 
 func _ready() -> void:
     title = "Humanoid Muscles"
-    size = Vector2(800, 600)
+    size = Vector2(1200, 600)
+    _setup_picker()
     _load_default_profile()
+    _load_model()
+    _populate_tree()
     _populate_list()
-    $VBox/Buttons/LoadButton.pressed.connect(_on_load_pressed)
-    $VBox/Buttons/SaveButton.pressed.connect(_on_save_pressed)
-    _load_dialog.file_selected.connect(_on_load_file_selected)
-    _save_dialog.file_selected.connect(_on_save_file_selected)
+
+func _setup_picker() -> void:
+    _picker.base_type = "MuscleProfile"
+    _picker.edited_resource = _profile
+    _picker.resource_changed.connect(_on_profile_changed)
+
+func _on_profile_changed(res: Resource) -> void:
+    if res:
+        _profile = res
+    else:
+        _profile = MuscleProfile.new()
+        _load_default_profile()
+    _populate_list()
+
+func _load_model() -> void:
+    for child in _viewport.get_children():
+        child.queue_free()
+    _model = HumanoidScene.instantiate()
+    _viewport.add_child(_model)
+    var cam := Camera3D.new()
+    cam.position = Vector3(0, 1.5, 3)
+    cam.look_at(Vector3.ZERO)
+    _viewport.add_child(cam)
+    _viewport.camera_3d = cam
+    var light := DirectionalLight3D.new()
+    light.rotation_degrees = Vector3(-45, -30, 0)
+    _viewport.add_child(light)
+
+func _populate_tree() -> void:
+    _tree.clear()
+    var root := _tree.create_item()
+    root.set_text(0, _model.name)
+    _add_tree_items(root, _model)
+
+func _add_tree_items(parent: TreeItem, node: Node) -> void:
+    for child in node.get_children():
+        var item := _tree.create_item(parent)
+        item.set_text(0, child.name)
+        _add_tree_items(item, child)
 
 func _load_default_profile() -> void:
     _profile.muscles.clear()
@@ -36,55 +76,17 @@ func _populate_list() -> void:
         var data = _profile.muscles[id]
         var row := HBoxContainer.new()
         _list.add_child(row)
-
-        var enabled := CheckBox.new()
-        enabled.button_pressed = data.get("enabled", true)
-        enabled.toggled.connect(func(v, id=id): _profile.muscles[id]["enabled"] = v)
-        row.add_child(enabled)
-
         var label := Label.new()
         label.text = "%s / %s" % [data.get("bone_ref", ""), data.get("axis", "")]
         label.size_flags_horizontal = Control.SIZE_EXPAND_FILL
         row.add_child(label)
 
-        var min_box := SpinBox.new()
-        min_box.min_value = -180.0
-        min_box.max_value = 180.0
-        min_box.step = 1.0
-        min_box.value = data.get("min_deg", 0.0)
-        min_box.value_changed.connect(func(v, id=id): _profile.muscles[id]["min_deg"] = v)
-        row.add_child(min_box)
+        var slider := HSlider.new()
+        slider.min_value = data.get("min_deg", -180.0)
+        slider.max_value = data.get("max_deg", 180.0)
+        slider.step = 1.0
+        slider.value = data.get("default_deg", 0.0)
+        slider.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+        slider.value_changed.connect(func(v, id=id): _profile.muscles[id]["default_deg"] = v)
+        row.add_child(slider)
 
-        var max_box := SpinBox.new()
-        max_box.min_value = -180.0
-        max_box.max_value = 180.0
-        max_box.step = 1.0
-        max_box.value = data.get("max_deg", 0.0)
-        max_box.value_changed.connect(func(v, id=id): _profile.muscles[id]["max_deg"] = v)
-        row.add_child(max_box)
-
-        var def_box := SpinBox.new()
-        def_box.min_value = -180.0
-        def_box.max_value = 180.0
-        def_box.step = 1.0
-        def_box.value = data.get("default_deg", 0.0)
-        def_box.value_changed.connect(func(v, id=id): _profile.muscles[id]["default_deg"] = v)
-        row.add_child(def_box)
-
-func _on_load_pressed() -> void:
-    _load_dialog.popup_centered()
-
-func _on_save_pressed() -> void:
-    _save_dialog.popup_centered()
-
-func _on_load_file_selected(path: String) -> void:
-    var file := FileAccess.open(path, FileAccess.READ)
-    if file:
-        var text := file.get_as_text()
-        _profile = MuscleIO.from_json(text)
-        _populate_list()
-
-func _on_save_file_selected(path: String) -> void:
-    var file := FileAccess.open(path, FileAccess.WRITE)
-    if file:
-        file.store_string(MuscleIO.to_json(_profile))
--- addons/puppet/profile_resource.gd
+++ addons/puppet/profile_resource.gd
@@ -2,6 +2,7 @@
 extends Resource
 class_name MuscleProfile
 
-## Resource storing muscle configuration values.
-@export var muscles := {}
+## Resource storing muscle configuration values for a humanoid avatar.
+@export var skeleton: NodePath
+@export var muscles: Dictionary = {}
 @export var version: String = "0.1"
