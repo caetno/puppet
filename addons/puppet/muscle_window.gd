fe6469afb88bba287821d77fd7727b3afb5ed36a
caetno <caetno@users.noreply.github.com>
2025-08-24 05:33:15 +0200

    Add humanoid bone mapping and profile loading

--- addons/puppet/muscle_window.gd
+++ addons/puppet/muscle_window.gd
@@ -1,9 +1,15 @@
 @tool
 extends Window
+class_name MuscleWindow
+const MuscleProfile = preload("res://addons/puppet/profile_resource.gd")
 
 ## Placeholder editor window for muscle configuration.
 var editor_plugin: EditorPlugin
+var _profile: MuscleProfile = MuscleProfile.new()
 
 func _ready() -> void:
     title = "Humanoid Muscles"
     size = Vector2(800, 600)
+
+func load_skeleton(skeleton: Skeleton3D) -> void:
+    _profile.load_from_skeleton(skeleton)
