a9e990758118feb6e5a40916f907440780f02b5a
caetno <caetno@users.noreply.github.com>
2025-08-24 05:33:25 +0200

    Refactor muscle window layout

--- addons/puppet/muscle_window.gd
+++ addons/puppet/muscle_window.gd
@@ -4,6 +4,10 @@ extends Window
 ## Placeholder editor window for muscle configuration.
 var editor_plugin: EditorPlugin
 
+@onready var _tree: Tree = $Split/Tree
+@onready var _viewport_container: SubViewportContainer = $Split/SubViewportContainer
+@onready var _list: VBoxContainer = $Split/PanelContainer/ScrollContainer/VBoxContainer
+
 func _ready() -> void:
     title = "Humanoid Muscles"
     size = Vector2(800, 600)
